3ff94594c7fe1109f2f9f8229d973c1e3a7886c1
NaknaoK <76167108+NaknaoK@users.noreply.github.com>
2024-01-09 00:00:32 +0900

    Update controller.js

--- controller.js
+++ controller.js
@@ -73,12 +73,15 @@ async function init() {
     // spotLight.position.set(10, 10, 10);
     // scene.add(spotLight);
 
-    const ambientLight = new THREE.AmbientLight(0xffffff, 2);
+    const ambientLight = new THREE.AmbientLight(0xffffff, 1);
     scene.add(ambientLight);
     //光源を作成
-		const light = new THREE.DirectionalLight( 0xffffff, 2 );
-		light.position.set( 2, 4, 2 );
+		const light = new THREE.DirectionalLight( 0xffffff, 2.5);
+		light.position.set( 200, 400, 200 );
 		scene.add( light );
+    const pointLight = new THREE.PointLight(0xffffff, 1);
+    pointLight.position.set( -400, 400, -100 );
+    scene.add(pointLight);
   }
   /* ----基本的な設定----- */
   /* ----Map関係---- */
